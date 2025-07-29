d986aa1549023f33bdd0a6fb0c02e6a65c983c9d
Muhammad Sabbir Hossain Bin Ahmad <dustobalok000@gmail.com>
2025-07-29 22:49:43 +0600

    Complete: Variables and constants in c++ ✅ 👍

---
+++ 01_varconst/main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int score;
+    score = 110;
+
+    int hiteshBalance = 500;
+    hiteshBalance = 1000;
+
+    const int uid = 232323;
+
+    cout << "Welcome to chai with cpp 1." << endl;
+    cout << "Welcome to chai with cpp 1." << endl;
+    cout << "Welcome to chai with cpp 1." << endl;
+
+    return 0;
+}

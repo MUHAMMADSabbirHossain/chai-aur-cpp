b9364e2da4d89308d6eab38838b2c796265f9377
Muhammad Sabbir Hossain Bin Ahmad <dustobalok000@gmail.com>
2025-07-29 22:08:36 +0600

    Add test_file_module.c and integrate with testc.c

---
+++ test/test_file_module.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int test_file_module_func()
+{
+    printf("test_file_module_func from another file.\n");
+    return 0;
+}
--- test/testc.c
+++ test/testc.c
@@ -1,7 +1,10 @@
 #include <stdio.h>
+#include "test_file_module.c"
 
 int main()
 {
     printf("hello world from C\n");
+
+    test_file_module_func();
     return 0;
 }

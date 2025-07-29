8b461ccdf24bf05bf21595328e13e6283037ddef
Muhammad Sabbir Hossain Bin Ahmad <dustobalok000@gmail.com>
2025-07-29 21:42:37 +0600

    Complete: Going in depth of Hello World | chai aur C++✅ 👍

---
+++ test/file_module.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+using namespace std;
+
+int file_module_func()
+{
+    cout << "file_module_func from another file." << endl;
+    return 0;
+}
--- test/hello.cpp
+++ test/hello.cpp
@@ -1,8 +1,30 @@
 #include <iostream>
+#include "file_module.cpp"
 
 using namespace std;
+using std::cout;
+using std::endl;
+
+// namespace mychai
+// {
+//     void display()
+//     {
+//         // comment
+//     }
+// }
 
 int main()
 {
-    cout << "Hello Chai from C++";
+    // mychai::display();
+
+    cout << "Hello Chai from C++.";
+    std::cout << "Hello Chai from C++." << std::endl;
+
+    file_module_func();
+    return 0;
+}
+
+int hitesh()
+{
+    return 0;
 }
